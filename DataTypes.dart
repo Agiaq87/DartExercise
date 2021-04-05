f93d0368a930aadeaf8ca7004857438709f4560a
Alessandro Giaquinto <a.giaquinto8@studenti.unisa.it>
2021-04-05 22:31:30 +0200

    Update DataTypes.dart

--- DataTypes.dart
+++ DataTypes.dart
@@ -6,8 +6,11 @@ void main() {
   String name = "Alessandro"; // Dichiarazione di variabile
   var surname = "Giaquinto";  // Type inference, secondo lo standard è preferibile
   dynamic age = "NAN";        // Tipo determinato a Runtime, penalizzazione sulle performance (non usare mai)
+  late var id;                // Lazy execution
+ 
 
   print("Hello, $name!"); // Interpolazione di stringa
   print("Hello, ${name.toUpperCase() + " " + surname.toLowerCase()}!"); // Interpolazione di espressioni
-  print("Dynamic $age");
+  print("Dynamic $age");  
+  
 }
