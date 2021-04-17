a7e2b1a672ef1147dab5b0e64d6bb9157069c009
Alessandro Giaquinto <a.giaquinto8@studenti.unisa.it>
2021-04-17 11:27:27 +0200

    Create DartEnums.dart

---
+++ DartEnums.dart
@@ -0,0 +1,9 @@
+enum Fruits { Apple, Pear, Grapes, Banana, Mango, Orange}
+
+void main() {
+  Fruits liked = Fruits.Apple;
+  var liked2 = Fruits.Orange;
+  
+  print(liked.toString());
+  print(liked.index);
+}
